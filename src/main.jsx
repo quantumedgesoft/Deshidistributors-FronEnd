a964ba4768a5cf7075e3f79afd1f633ed59471c4
Jewel <jewelhfahim@gmail.com>
2025-02-23 12:14:57 +0600

    routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './style.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './bootstrap.min.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./bootstrap.min.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ src/routes/Router.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router";
+import About from "../pages/about/About";
+import Home from "../pages/home/Home";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default Router;
