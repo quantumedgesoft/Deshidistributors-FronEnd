febd5436b7ad02901d8a166f150393982296f6ea
Jewel <jewelhfahim@gmail.com>
2025-02-25 13:14:59 +0600

    upcomming

--- src/components/sliders/VegetableSlider.jsx
+++ src/components/sliders/VegetableSlider.jsx
@@ -5,24 +5,193 @@ import "swiper/css/navigation";
 import { Autoplay } from "swiper/modules";
 
 export default function VegetableSlidder() {
-  
-  // const dataas = [
-  //   {
-  //     id: 1,
-  //     label: "Vegetable",
-  //     img: "/src/assets/img/vegetable-item-6.jpg",
-  //     title: "Vegetable",
-  //     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt"
-  //   },
-  //   {
-  //     id: 2,
-  //     label: "Vegetable",
-  //     img: "/src/assets/img/vegetable-item-6.jpg",
-  //     title: "Vegetable",
-  //     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt"
-  //   },
+  const products = [
+    {
+      id: 1,
+      category: "best",
+      title: "Rice",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+      img: "/img/products/rice.jpg",
+    },
+    {
+      id: 2,
+      category: "recent",
+      title: "Daal",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+      img: "/img/products/Daal.jpeg",
+    },
+    {
+      id: 3,
+      category: "popular",
+      title: "Flour",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+      img: "/img/products/Flour.jpeg",
+    },
+    {
+      id: 4,
+      category: "best",
+      title: "Edible Oil",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+      img: "/img/products/Edible Oil.jpg",
+    },
+    {
+      id: 5,
+      category: "recent",
+      title: "Nuts",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+      img: "/img/products/Nuts.jpg",
+    },
+    {
+      id: 6,
+      category: "popular",
+      title: "Spices & Herbs",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+      img: "/img/products/Spices & Herbs.png",
+    },
+    {
+      id: 7,
+      category: "best",
+      title: "Sugar",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+      img: "/img/products/Sugar.jpg",
+    },
+    {
+      id: 8,
+      category: "recent",
+      title: "Salt",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+      img: "/img/products/Salt.png",
+    },
+    {
+      id: 9,
+      category: "popular",
+      title: "Ghee",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+      img: "/img/products/Ghee.png",
+    },
+    {
+      id: 10,
+      category: "best",
+      title: "Pickle",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+      img: "/img/products/Pickle.jpg",
+    },
+    {
+      id: 11,
+      category: "recent",
+      title: "Soup",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+      img: "/img/products/Soup.jpg",
+    },
+    {
+      id: 12,
+      category: "popular",
+      title: "Soup",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+      img: "/img/products/Noodles.jpeg",
+    },
 
-  // ]
+    // {
+    //   id: 13,
+    //   category: "Noodles",
+    //   title: "Chanachur",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    //     img: "/img/products/Chanachur.png",
+    // },
+    // {
+    //   id: 14,
+    //   category: "Noodles",
+    //   title: "Dates",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    //     img: "/img/products/Dates.png",
+    // },
+    // {
+    //   id: 15,
+    //   category: "Noodles",
+    //   title: "Biscuit",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    //     img: "/img/products/Biscuit.jpg",
+    // },
+    // {
+    //   id: 16,
+    //   category: "Noodles",
+    //   title: "Sauce",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    //     img: "/img/products/Sauce.jpg",
+    // },
+    // {
+    //   id: 17,
+    //   category: "Noodles",
+    //   title: "Honey",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    //     img: "/img/products/Honey.png",
+    // },
+    // {
+    //   id: 18,
+    //   category: "Noodles",
+    //   title: "Tea, Coffee, Milk Drinks",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    //     img: "/img/products/Tea, Coffee, Milk Drinks.png",
+    // },
+    // {
+    //   id: 19,
+    //   category: "Noodles",
+    //   title: "Candy",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    //     img: "/img/products/Candy.jpg",
+    // },
+    // {
+    //   id: 20,
+    //   category: "Noodles",
+    //   title: "Paan",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    //     img: "/img/products/Paan.png",
+    // },
+    // {
+    //   id: 21,
+    //   category: "Noodles",
+    //   title: "Healthcare & Beauty",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    //     img: "/img/products/Healthcare & Beauty.jpg",
+    // },
+    // {
+    //   id: 22,
+    //   category: "Noodles",
+    //   title: "Soap",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    //     img: "/img/products/Soap.png",
+    // },
+    // {
+    //   id: 23,
+    //   category: "Noodles",
+    //   title: "Miscellaneous",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt",
+    //     img: "/img/products/Miscellaneous.jpg",
+    // },
+  ];
 
   return (
     <>
@@ -40,320 +209,34 @@ export default function VegetableSlidder() {
         modules={[Autoplay]}
         className=""
       >
-        <SwiperSlide>
-          <div className="border border-primary rounded position-relative vesitable-item">
-            <div className="vesitable-img">
-              <img
-                src="/src/assets/img/vegetable-item-1.jpg"
-                className="img-fluid w-100 rounded-top"
-                alt=""
-              />
-            </div>
-            <div
-              className="text-white bg-primary px-3 py-1 rounded position-absolute"
-              style={{ top: "10px", right: "10px" }}
-            >
-              Vegetable
-            </div>
-            <div className="p-4 rounded-bottom">
-              <h4>Parsely</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
-                eiusmod te incididunt
-              </p>
-              <div className="d-flex justify-content-between flex-lg-wrap">
-                <p className="text-dark fs-5 fw-bold mb-0">$4.99 / kg</p>
-                <a
-                  href="#"
-                  className="btn border border-secondary rounded-pill px-3 text-primary"
-                >
-                  <i className="fa fa-shopping-bag me-2 text-primary"></i> Add
-                  to cart
-                </a>
+        {products.map((item) => (
+          <SwiperSlide key={item.id}>
+            <div className="border border-primary rounded position-relative vesitable-item">
+              <div className="vesitable-img">
+                <img
+                  src={item.img}
+                  className="img-fluid w-100 rounded-top"
+                  style={{maxHeight: "250px", minHeight: "250px"}}
+                  alt=""
+                />
               </div>
-            </div>
-          </div>
-        </SwiperSlide>
+              <div
+                className="text-white bg-primary px-3 py-1 rounded position-absolute"
+                style={{ top: "10px", right: "10px" }}
+              >
+                Comming...
+              </div>
+              <div className="p-4 rounded-bottom">
+                <h4>{item.title}</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                  eiusmod te incididunt
+                </p>
 
-        <SwiperSlide>
-          <div className="border border-primary rounded position-relative vesitable-item">
-            <div className="vesitable-img">
-              <img
-                src="/src/assets/img/vegetable-item-6.jpg"
-                className="img-fluid w-100 rounded-top"
-                alt=""
-              />
-            </div>
-            <div
-              className="text-white bg-primary px-3 py-1 rounded position-absolute"
-              style={{ top: "10px", right: "10px" }}
-            >
-              Vegetable
-            </div>
-            <div className="p-4 rounded-bottom">
-              <h4>Parsely</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
-                eiusmod te incididunt
-              </p>
-              <div className="d-flex justify-content-between flex-lg-wrap">
-                <p className="text-dark fs-5 fw-bold mb-0">$4.99 / kg</p>
-                <a
-                  href="#"
-                  className="btn border border-secondary rounded-pill px-3 text-primary"
-                >
-                  <i className="fa fa-shopping-bag me-2 text-primary"></i> Add
-                  to cart
-                </a>
               </div>
             </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="border border-primary rounded position-relative vesitable-item">
-            <div className="vesitable-img">
-              <img
-                src="/src/assets/img/vegetable-item-1.jpg"
-                className="img-fluid w-100 rounded-top"
-                alt=""
-              />
-            </div>
-            <div
-              className="text-white bg-primary px-3 py-1 rounded position-absolute"
-              style={{ top: "10px", right: "10px" }}
-            >
-              Vegetable
-            </div>
-            <div className="p-4 rounded-bottom">
-              <h4>Parsely</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
-                eiusmod te incididunt
-              </p>
-              <div className="d-flex justify-content-between flex-lg-wrap">
-                <p className="text-dark fs-5 fw-bold mb-0">$4.99 / kg</p>
-                <a
-                  href="#"
-                  className="btn border border-secondary rounded-pill px-3 text-primary"
-                >
-                  <i className="fa fa-shopping-bag me-2 text-primary"></i> Add
-                  to cart
-                </a>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="border border-primary rounded position-relative vesitable-item">
-            <div className="vesitable-img">
-              <img
-                src="/src/assets/img/vegetable-item-3.png"
-                className="img-fluid w-100 rounded-top bg-light"
-                alt=""
-              />
-            </div>
-            <div
-              className="text-white bg-primary px-3 py-1 rounded position-absolute"
-              style={{ top: "10px", right: "10px" }}
-            >
-              Vegetable
-            </div>
-            <div className="p-4 rounded-bottom">
-              <h4>Banana</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
-                eiusmod te incididunt
-              </p>
-              <div className="d-flex justify-content-between flex-lg-wrap">
-                <p className="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
-                <a
-                  href="#"
-                  className="btn border border-secondary rounded-pill px-3 text-primary"
-                >
-                  <i className="fa fa-shopping-bag me-2 text-primary"></i> Add
-                  to cart
-                </a>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="border border-primary rounded position-relative vesitable-item">
-            <div className="vesitable-img">
-              <img
-                src="/src/assets/img/vegetable-item-4.jpg"
-                className="img-fluid w-100 rounded-top"
-                alt=""
-              />
-            </div>
-            <div
-              className="text-white bg-primary px-3 py-1 rounded position-absolute"
-              style={{ top: "10px", right: "10px" }}
-            >
-              Vegetable
-            </div>
-            <div className="p-4 rounded-bottom">
-              <h4>Bell Papper</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
-                eiusmod te incididunt
-              </p>
-              <div className="d-flex justify-content-between flex-lg-wrap">
-                <p className="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
-                <a
-                  href="#"
-                  className="btn border border-secondary rounded-pill px-3 text-primary"
-                >
-                  <i className="fa fa-shopping-bag me-2 text-primary"></i> Add
-                  to cart
-                </a>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="border border-primary rounded position-relative vesitable-item">
-            <div className="vesitable-img">
-              <img
-                src="/src/assets/img/vegetable-item-5.jpg"
-                className="img-fluid w-100 rounded-top"
-                alt=""
-              />
-            </div>
-            <div
-              className="text-white bg-primary px-3 py-1 rounded position-absolute"
-              style={{ top: "10px", right: "10px" }}
-            >
-              Vegetable
-            </div>
-            <div className="p-4 rounded-bottom">
-              <h4>Potatoes</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
-                eiusmod te incididunt
-              </p>
-              <div className="d-flex justify-content-between flex-lg-wrap">
-                <p className="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
-                <a
-                  href="#"
-                  className="btn border border-secondary rounded-pill px-3 text-primary"
-                >
-                  <i className="fa fa-shopping-bag me-2 text-primary"></i> Add
-                  to cart
-                </a>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="border border-primary rounded position-relative vesitable-item">
-            <div className="vesitable-img">
-              <img
-                src="/src/assets/img/vegetable-item-6.jpg"
-                className="img-fluid w-100 rounded-top"
-                alt=""
-              />
-            </div>
-            <div
-              className="text-white bg-primary px-3 py-1 rounded position-absolute"
-              style={{ top: "10px", right: "10px" }}
-            >
-              Vegetable
-            </div>
-            <div className="p-4 rounded-bottom">
-              <h4>Parsely</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
-                eiusmod te incididunt
-              </p>
-              <div className="d-flex justify-content-between flex-lg-wrap">
-                <p className="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
-                <a
-                  href="#"
-                  className="btn border border-secondary rounded-pill px-3 text-primary"
-                >
-                  <i className="fa fa-shopping-bag me-2 text-primary"></i> Add
-                  to cart
-                </a>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="border border-primary rounded position-relative vesitable-item">
-            <div className="vesitable-img">
-              <img
-                src="/src/assets/img/vegetable-item-5.jpg"
-                className="img-fluid w-100 rounded-top"
-                alt=""
-              />
-            </div>
-            <div
-              className="text-white bg-primary px-3 py-1 rounded position-absolute"
-              style={{ top: "10px", right: "10px" }}
-            >
-              Vegetable
-            </div>
-            <div className="p-4 rounded-bottom">
-              <h4>Potatoes</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
-                eiusmod te incididunt
-              </p>
-              <div className="d-flex justify-content-between flex-lg-wrap">
-                <p className="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
-                <a
-                  href="#"
-                  className="btn border border-secondary rounded-pill px-3 text-primary"
-                >
-                  <i className="fa fa-shopping-bag me-2 text-primary"></i> Add
-                  to cart
-                </a>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="border border-primary rounded position-relative vesitable-item">
-            <div className="vesitable-img">
-              <img
-                src="/src/assets/img/vegetable-item-6.jpg"
-                className="img-fluid w-100 rounded-top"
-                alt=""
-              />
-            </div>
-            <div
-              className="text-white bg-primary px-3 py-1 rounded position-absolute"
-              style={{ top: "10px", right: "10px" }}
-            >
-              Vegetable
-            </div>
-            <div className="p-4 rounded-bottom">
-              <h4>Parsely</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
-                eiusmod te incididunt
-              </p>
-              <div className="d-flex justify-content-between flex-lg-wrap">
-                <p className="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
-                <a
-                  href="#"
-                  className="btn border border-secondary rounded-pill px-3 text-primary"
-                >
-                  <i className="fa fa-shopping-bag me-2 text-primary"></i> Add
-                  to cart
-                </a>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
