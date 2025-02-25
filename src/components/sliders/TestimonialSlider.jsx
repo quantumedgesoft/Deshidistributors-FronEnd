fa9464d6a18f19855d36a26e04f06cc661866b96
Sakib <sakib.quantumdev@gmail.com>
2025-02-25 11:15:04 +0600

    testimonial section added

--- src/components/sliders/TestimonialSlider.jsx
+++ src/components/sliders/TestimonialSlider.jsx
@@ -3,9 +3,38 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Autoplay } from "swiper/modules";
-
-export default function TestimonialSlidder() {
-
+import customerImg from "/src/assets/img/testimonial-1.jpg";
+import avatar from "/src/assets/img/avatar.jpg"
+export default function TestimonialSlider() {
+  const CustomerReview = [
+    {
+      id: 1,
+      name: "Harpoon Load",
+      Profession: "Employee",
+      ImageUrl: customerImg,
+      reviewText:
+        "Lorem Ipsum is simply dummy text of the printing Ipsum has been the industry's standard dummy text ever since the 1500s",
+      rating: 5,
+    },
+    {
+      id: 2,
+      name: "Jane Doe",
+      Profession: "Designer",
+      ImageUrl: avatar,
+      reviewText:
+        "Great experience! The quality of service was top-notch. Highly recommended!",
+      rating: 4,
+    },
+    {
+      id: 3,
+      name: "John Smith",
+      Profession: "Developer",
+      ImageUrl: avatar,
+      reviewText:
+        "Fantastic work! Really satisfied with the professionalism and expertise.",
+      rating: 3,
+    },
+  ];
 
   return (
     <>
@@ -23,122 +52,43 @@ export default function TestimonialSlidder() {
         modules={[Autoplay]}
         className=""
       >
-        <SwiperSlide>
-          <div className="testimonial-item img-border-radius bg-light rounded p-4">
-            <div className="position-relative">
-              <i
-                className="fa fa-quote-right fa-2x text-secondary position-absolute"
-                style={{ bottom: "30px", right: "0" }}
-              ></i>
-              <div className="mb-4 pb-4 border-bottom border-secondary">
-                <p className="mb-0">
-                  Lorem Ipsum is simply dummy text of the printing Ipsum has
-                  been the industry&apos;s standard dummy text ever since the
-                  1500s,
-                </p>
-              </div>
-              <div className="d-flex align-items-center flex-nowrap">
-                <div className="bg-secondary rounded">
-                  <img
-                    src="/src/assets/img/testimonial-1.jpg"
-                    className="img-fluid rounded"
-                    style={{ width: "100px", height: "100px" }}
-                    alt=""
-                  />
+        {CustomerReview.map((review, index) => (
+          <SwiperSlide key={review.id}>
+            <div className="testimonial-item img-border-radius bg-light rounded p-4">
+              <div className="position-relative">
+                <i
+                  className="fa fa-quote-right fa-2x text-secondary position-absolute"
+                  style={{ bottom: "30px", right: "0" }}
+                ></i>
+                <div className="mb-4 pb-4 border-bottom border-secondary">
+                  <p className="mb-0">{review.reviewText}</p>
                 </div>
-                <div className="ms-4 d-block">
-                  <h4 className="text-dark">Client Name</h4>
-                  <p className="m-0 pb-3">Profession</p>
-                  <div className="d-flex pe-5">
-                    <i className="fas fa-star text-primary"></i>
-                    <i className="fas fa-star text-primary"></i>
-                    <i className="fas fa-star text-primary"></i>
-                    <i className="fas fa-star text-primary"></i>
-                    <i className="fas fa-star"></i>
+                <div className="d-flex align-items-center flex-nowrap">
+                  <div className="bg-secondary rounded">
+                    <img
+                      src={review.ImageUrl}
+                      className="img-fluid rounded"
+                      style={{ width: "100px", height: "100px" }}
+                      alt=""
+                    />
+                  </div>
+                  <div className="ms-4 d-block">
+                    <h4 className="text-dark">{review.name}</h4>
+                    <p className="m-0 pb-3">{review.Profession}</p>
+                    <div className="d-flex pe-5">
+                      {[...Array(review.rating)].map((_, i) => (
+                        <i key={i} className="fas fa-star text-secondary"></i>
+                      ))}
+                      {[...Array(5 - review.rating)].map((_, i) => (
+                        <i key={i} className="fas fa-star "></i>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="testimonial-item img-border-radius bg-light rounded p-4">
-            <div className="position-relative">
-              <i
-                className="fa fa-quote-right fa-2x text-secondary position-absolute"
-                style={{ bottom: "30px", right: "0" }}
-              ></i>
-              <div className="mb-4 pb-4 border-bottom border-secondary">
-                <p className="mb-0">
-                  Lorem Ipsum is simply dummy text of the printing Ipsum has
-                  been the industry&apos;s standard dummy text ever since the
-                  1500s,
-                </p>
-              </div>
-              <div className="d-flex align-items-center flex-nowrap">
-                <div className="bg-secondary rounded">
-                  <img
-                    src="/src/assets/img/testimonial-1.jpg"
-                    className="img-fluid rounded"
-                    style={{ width: "100px", height: "100px" }}
-                    alt=""
-                  />
-                </div>
-                <div className="ms-4 d-block">
-                  <h4 className="text-dark">Client Name</h4>
-                  <p className="m-0 pb-3">Profession</p>
-                  <div className="d-flex pe-5">
-                    <i className="fas fa-star text-primary"></i>
-                    <i className="fas fa-star text-primary"></i>
-                    <i className="fas fa-star text-primary"></i>
-                    <i className="fas fa-star text-primary"></i>
-                    <i className="fas fa-star text-primary"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="testimonial-item img-border-radius bg-light rounded p-4">
-            <div className="position-relative">
-              <i
-                className="fa fa-quote-right fa-2x text-secondary position-absolute"
-                style={{ bottom: "30px", right: "0" }}
-              ></i>
-              <div className="mb-4 pb-4 border-bottom border-secondary">
-                <p className="mb-0">
-                  Lorem Ipsum is simply dummy text of the printing Ipsum has
-                  been the industry&apos;s standard dummy text ever since the
-                  1500s,
-                </p>
-              </div>
-              <div className="d-flex align-items-center flex-nowrap">
-                <div className="bg-secondary rounded">
-                  <img
-                    src="/src/assets/img/testimonial-1.jpg"
-                    className="img-fluid rounded"
-                    style={{ width: "100px", height: "100px" }}
-                    alt=""
-                  />
-                </div>
-                <div className="ms-4 d-block">
-                  <h4 className="text-dark">Client Name</h4>
-                  <p className="m-0 pb-3">Profession</p>
-                  <div className="d-flex pe-5">
-                    <i className="fas fa-star text-primary"></i>
-                    <i className="fas fa-star text-primary"></i>
-                    <i className="fas fa-star text-primary"></i>
-                    <i className="fas fa-star text-primary"></i>
-                    <i className="fas fa-star text-primary"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
