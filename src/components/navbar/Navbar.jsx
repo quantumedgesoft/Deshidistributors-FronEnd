f2ea22edff233fdf64bd104ee8e20733bc834a23
Sakib <sakib.quantumdev@gmail.com>
2025-02-27 11:43:36 +0600

    navbar updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,31 +1,43 @@
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import Topbar from "./Topbar";
 import logo from "../../assets/Images/deshidistributors_logo.png";
+
 const Navbar = () => {
-  const categories = [
-    "Snacks",
-    "Pickle",
-    "Miscellaneous",
-    "Soap",
-    "Healthcare & Beauty",
-    "Paan",
-    "Candy",
-    "Tea, Coffee, Milk Drinks",
-    "Honey",
-    "Sauce",
-    "Biscuit",
-    "Dates",
-    "Chanachur",
-    "Noodles",
-    "Ghee",
-    "Salt",
-    "Sugar",
-    "Spices & Herb",
-    "Nuts",
-    "Edible Oil",
-    "Flour",
-    "Daal",
-    "Rice",
+  const menuItems = [
+    { name: "Home", link: "/" },
+    { name: "About", link: "/about" },
+    {
+      name: "Products",
+      link: "/products",
+      dropdown: true,
+      categories: [
+        "Snacks",
+        "Pickle",
+        "Miscellaneous",
+        "Soap",
+        "Healthcare & Beauty",
+        "Paan",
+        "Candy",
+        "Tea, Coffee, Milk Drinks",
+        "Honey",
+        "Sauce",
+        "Biscuit",
+        "Dates",
+        "Chanachur",
+        "Noodles",
+        "Ghee",
+        "Salt",
+        "Sugar",
+        "Spices & Herb",
+        "Nuts",
+        "Edible Oil",
+        "Flour",
+        "Daal",
+        "Rice",
+      ],
+    },
+    { name: "Operation Map", link: "/operation-map" },
+    { name: "Contact", link: "/contact" },
   ];
 
   return (
@@ -34,10 +46,12 @@ const Navbar = () => {
         <Topbar />
         <div className="container px-0">
           <nav className="navbar navbar-light bg-white navbar-expand-xl">
+            {/* Logo */}
             <Link to="/" className="navbar-brand">
-              <img src={logo} alt="" className="" style={{ width: "150px" }} />
+              <img src={logo} alt="Deshi Distributors" style={{ width: "150px" }} />
             </Link>
 
+            {/* Mobile Toggle Button */}
             <button
               className="navbar-toggler py-2 px-3"
               type="button"
@@ -47,59 +61,31 @@ const Navbar = () => {
               <span className="fa fa-bars text-primary"></span>
             </button>
 
-            {/* <MobileSideMenu categories={categories} /> */}
-
-            <div
-              className="collapse navbar-collapse bg-white"
-              id="navbarCollapse"
-              style={{ position: "relative" }}
-            >
+            <div className="collapse navbar-collapse bg-white" id="navbarCollapse">
               <div className="navbar-nav mx-auto">
-                <Link to="/" className="nav-item nav-link active">
-                  Home
-                </Link>
-                <Link to="/about" className="nav-item nav-link">
-                  About
-                </Link>
-
-                <div className="nav-item">
-                  <Link
-                    href="/"
-                    className="nav-link dropdown-toggle"
-                    data-bs-toggle="dropdown"
-                  >
-                    Products
-                  </Link>
-
-                  <div
-                    className="dropdown-menu m-0 bg-secondary rounded-0 overflow-auto"
-                    style={{ maxHeight: "40vh" }}
-                  >
-                    {categories.map((item, idx) => (
-                      <Link
-                        key={idx}
-                        href="cart.html"
-                        className="dropdown-item"
-                      >
-                        {item}
+                {menuItems.map((item, idx) =>
+                  item.dropdown ? (
+                    <div className="nav-item dropdown" key={idx}>
+                      <Link to={item.link} className="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                        {item.name}
                       </Link>
-                    ))}
-                  </div>
-                </div>
-
-                <Link to="/contact" className="nav-item nav-link">
-                  Operation Map
-                </Link>
-                <Link to="/contact" className="nav-item nav-link">
-                  Contact
-                </Link>
+                      <div className="dropdown-menu m-0 bg-secondary rounded-0 overflow-auto" style={{ maxHeight: "40vh" }}>
+                        <Link to={item.link} className="dropdown-item">All Products</Link>
+                        {item.categories.map((category, catIdx) => (
+                          <Link key={catIdx} to={`/products/${category.toLowerCase().replace(/\s+/g, "-")}`} className="dropdown-item">
+                            {category}
+                          </Link>
+                        ))}
+                      </div>
+                    </div>
+                  ) : (
+                    <Link key={idx} to={item.link} className="nav-item nav-link">
+                      {item.name}
+                    </Link>
+                  )
+                )}
               </div>
-
-              <button
-                type="submit"
-                className="btn btn-primary border-2 border-secondary py-2 px-4  rounded-pill text-white h-100"
-                style={{ top: "0", right: "25%" }}
-              >
+              <button type="button" className="btn btn-primary border-secondary py-2 px-4 rounded-pill text-white">
                 Career
               </button>
             </div>
